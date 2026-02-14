9bec2ff064c020bfe49b0568cb686cf926ac9eee
range3 <white.line.oar@gmail.com>
2026-02-14 11:19:26 +0900

    Update documentation

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-41320930.js',
+                path_to_root + 'searchindex-dc74ca0b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
