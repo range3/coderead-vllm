2474a4f2dae30f462fbc68e1a90ee8c7415e623a
range3 <white.line.oar@gmail.com>
2026-02-16 00:13:41 +0900

    Update documentation

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b4930b74.js',
+                path_to_root + 'searchindex-0ac39c97.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
